e8a70e2414ebffd50f72d8b5aa8e7bd53bb33dff
Byron Chang <thebyronc@gmail.com>
2018-06-29 12:06:35 -0700

    new head before fb

---
+++ src/constants/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNJ9zZBDuZK089PXTXRj4OyKv9hFCFZLc",
+    authDomain: "billfoil-a22de.firebaseapp.com",
+    databaseURL: "https://billfoil-a22de.firebaseio.com",
+    projectId: "billfoil-a22de",
+    storageBucket: "billfoil-a22de.appspot.com",
+    messagingSenderId: "564198582002"
+};
+
+export default firebaseConfig;
